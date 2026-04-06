ce95017bbaff589f207a3c43ad5fa02502f4ecc4
Delia Fraga <deliafraga01@gmail.com>
2026-04-06 19:27:16 +0200

    working on pipeline page frontend

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home_page():
     return render_template("homepage.html")
 
+
 @app.route("/resources")
 def resources_page():
     return render_template("resources.html")
+
+
+@app.route("/upload-images")
+def run_pipeline_page():
+    return render_template("pipeline-upload.html")
+
+
+@app.route("/run-pipeline", methods=["GET", "POST"])
+def submit_pipeline():
+    if request.method == "POST":
+        return render_template("pipeline-result.html")
+
+    # If someone just types the URL in manually (GET),
+    # redirect them back to the upload page.
+    return redirect(url_for('run_pipeline_page'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
